854feeb56d8b86310e6cc3c184f85e36ce3036d3
sungshot99 <fakhri.r.13@gmail.com>
2021-05-20 19:11:00 +0700

    Progress 20 Mei 2021 Bikin back-end button open.

--- src/TextEditor/Controller.java
+++ src/TextEditor/Controller.java
@@ -1,11 +1,21 @@
 package TextEditor;
 
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
 
 public class Controller implements Initializable {
     @FXML
@@ -32,6 +42,67 @@ public class Controller implements Initializable {
     private CheckMenuItem font_menu_times_new_roman;
     @FXML
     private CheckMenuItem font_menu_comic_sans;
+
+    public void chooseFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        //only allow text files to be selected using chooser
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt")
+        );
+        //set initial directory somewhere user will recognise
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        //let user select file
+        File fileToLoad = fileChooser.showOpenDialog(null);
+        //if file has been chosen, load it using asynchronous method (define later)
+        if(fileToLoad != null){
+            loadFileToTextArea(fileToLoad);
+        }
+    }
+
+    private Task<String> fileLoaderTask(File fileToLoad){
+        //Create a task to load the file asynchronously
+        Task<String> loadFileTask = new Task<>() {
+            @Override
+            protected String call() throws Exception {
+                BufferedReader reader = new BufferedReader(new FileReader(fileToLoad));
+                //Use Files.lines() to calculate total lines - used for progress
+                long lineCount;
+                try (Stream<String> stream = Files.lines(fileToLoad.toPath())) {
+                    lineCount = stream.count();
+                }
+                //Load in all lines one by one into a StringBuilder separated by "\n" - compatible with TextArea
+                String line;
+                StringBuilder totalFile = new StringBuilder();
+                long linesLoaded = 0;
+                while((line = reader.readLine()) != null) {
+                    totalFile.append(line);
+                    totalFile.append("\n");
+                    updateProgress(++linesLoaded, lineCount);
+                }
+                return totalFile.toString();
+            }
+        };
+        //If successful, update the text area, display a success message and store the loaded file reference
+        loadFileTask.setOnSucceeded(workerStateEvent -> {
+            try {
+                txtArea_main.setText(loadFileTask.get());
+                label_notif.setText("File loaded: " + fileToLoad.getName());
+            } catch (InterruptedException | ExecutionException e) {
+                txtArea_main.setText("Could not load file from:\n " + fileToLoad.getAbsolutePath());
+            }
+        });
+        //If unsuccessful, set text area with error message and status message to failed
+        loadFileTask.setOnFailed(workerStateEvent -> {
+            txtArea_main.setText("Could not load file from:\n " + fileToLoad.getAbsolutePath());
+            label_notif.setText("Failed to load file");
+        });
+        return loadFileTask;
+    }
+
+    private void loadFileToTextArea(File fileToLoad) {
+        Task<String> loadFileTask = fileLoaderTask(fileToLoad);
+        loadFileTask.run();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
